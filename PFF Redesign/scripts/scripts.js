efcdb9591bb8d9cf7ce7ea1e95ec8e514bd06536
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2016-04-16 21:33:07 -0400

    edits
    edits

--- PFF Redesign/scripts/scripts.js
+++ PFF Redesign/scripts/scripts.js
@@ -31,21 +31,21 @@ $(document).ready(function(){
         // other options
         gallery:{enabled:true}
     });
-    $('.paramount').magnificPopup({
+    $('.paramountp').magnificPopup({
         items: {
             src: '<div class="white-popup"><h4>The Paramount Theater</h4><h2>Events</h2><table><thead><tr><th>Time</th><th>Date</th><th>Event</th><th>Info</th></tr></thead><tbody><tr><td>8:00 pm</td><td>May 13</td><td>Screening</td><td>Special Selection: But Not For Me</td></tr><tr><td>12:00 pm</td><td>May 14</td><td>Workshop</td><td>Workshop with Impolite Company</td></tr><tr><td>2 - 6 pm</td><td>May 14</td><td>Screening</td><td>Screening of Official Selections</td></tr><tr><td>8:00 pm</td><td>May 14</td><td>Screening</td><td>Special Selection: La Jaula De Oro</td></tr></tbody></table><a href="location.html#paramount" title="Location Info" target="_blank">More Info</a></div>',
             type: 'inline'
         },
         closeBtnInside: true
     });
-    $('.field').magnificPopup({
+    $('.fieldp').magnificPopup({
         items: {
             src: '<div class="white-popup"><h4>Peekskill Coffee House</h4><h2>Events</h2><table><thead><tr><th>Time</th><th>Date</th><th>Event</th><th>Info</th></tr></thead><tbody><tr><td>5:00 pm</td><td>May 15</td><td>Screening</td><td>Official Selection Screening</td></tr></tbody></table><a href="location.html#field" title="Location Info" target="_blank">More Info</a></div>',
             type: 'inline'
         },
         closeBtnInside: true
     });
-    $('.hvcca').magnificPopup({
+    $('.hvccap').magnificPopup({
         items: {
             src: '<div class="white-popup"><h4>The Hudson Valley Center For Contemporary Art</h4><h2>Events</h2><table><thead><tr><th>Time</th><th>Date</th><th>Event</th><th>Info</th></tr></thead><tbody><tr><td>12 - 4 pm</td><td>May 15</td><td>Screening</td><td>Screening of Official Selections</td></tr></tbody></table><a href="location.html#hvcca" title="Location Info" target="_blank">More Info</a></div>',
             type: 'inline'
@@ -68,6 +68,59 @@ $(document).ready(function(){
         //Close parent of link.
         $(this).parent().slideUp();
     });
+
+    // ---------- Drop Down ---------- //
+
+    //Function to add hover event
+    function addHov(){
+        $('.navbar li').hover(function(e){
+            //Fade in the children of hover li element
+            $(this).children('ul').fadeIn();},
+            function(){
+                //This stops the menu from adding too many hover events in the queue if someone hovers on/off rapidly
+                $('ul',this).stop(true,true).fadeOut();
+        });
+    }
+
+    //Function to add Click event
+    function addClick(){
+        //Add click event to smallNav, which replaces the main nav on screens less than 750px
+        $('#smallNav li').on('click', function(e) {
+            //Use stopPropagation to prevent click from traveling up the DOM and closing entire menu
+            e.stopPropagation();
+            //Slide toggle children of li item
+            $(this).children('ul').slideToggle('slow');
+        });
+    }
+
+    // Check initial window width and add event handler for Nav
+    if($(window).width()>750){
+        addHov();
+    }
+    else{
+        addClick();
+    }
+
+    // Check window width on resize,remove previous events, and add new event
+    $(window).resize(function(e) {
+        //Window greater than 750px: remove event and call addHov
+        if($(window).width() > 750){
+            $('.navbar li').off();
+            addHov();
+        }
+        //Under 750px: remove event and call addClick
+        else{
+            $('.navbar li').off();
+            addClick();
+        }
+    });
+    //Close small Nav if click outside of menu
+    $('.cta, .container').on('click',function(e){
+        e.preventDefault();
+        //Slide up menu
+        $('.mobile ul').slideUp();
+    });
+
 
 
     /*---------- Sponsor Info ----------*/
@@ -136,4 +189,7 @@ $(document).ready(function(){
         });
     };
     sponsorPrint();
+    $('.logo').on('click', function(){
+        window.location = 'index.html';
+    });
 })
